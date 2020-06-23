8f87599b15891b828cb47b5f612f485bf76dff5b
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-23 14:15:58 +0530

    connecting to mongo via mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://admin:admin@cluster0-9jxxd.gcp.mongodb.net/mern_shopping?retryWrites=true&w=majority'
+};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 // const bodyParser = require('body-parser'); included in express.json
 
 //creating express server
@@ -8,7 +8,37 @@ const app = express();
 // Bodyparser Middleware
 // app.use(bodyParser.json());
 
-app.use(express.json());    //to parse json
+app.use(express.json()); //to parse json
+
+//DB Config
+//uri is supposed to be got from mongo atlas
+const db = require("./config/keys").mongoURI;
+
+//Connect to Mongo
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((error) => {
+    console.log("mondb not connected");
+    console.log(error);
+  });
+
+// mongoose.connect(db, {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+// });
+
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established successfully");
+// });
 
 const port = process.env.PORT || 5000;
 
