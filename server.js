98c28eef1c085e2602b40d44268e157585c3bfdd
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-27 12:45:12 +0530

    deploying on heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 // const bodyParser = require('body-parser'); included in express.json
 
 const items = require("./routes/api/items");
@@ -44,6 +45,16 @@ mongoose
 
 //use routes (app.use(path, variable it should refer to))
 app.use("/api/items", items);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
